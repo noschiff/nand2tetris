97feb66abdc1f7554e2eb6920489d7c3f9ad88d7
noschiff <noah@wow.com>
2020-03-14 17:10:09 -0400

    wrote mult

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,32 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+//initialize R2 to 0
+@R2
+M=0
+
+(LOOP)
+//if R1 is 0, end
+@R1
+D=M
+@END
+D;JEQ
+
+//add R0 to the sum (R2)
+@R0
+D=M
+@R2
+M=M+D
+
+//reduce R1 by 1
+@R1
+M=M-1
+
+//loop
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
