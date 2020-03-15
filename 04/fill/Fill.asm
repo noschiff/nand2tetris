f5ad40c78cb2523e3dbd182f7d880d4dd861a7fe
noschiff <noah@wow.com>
2020-03-14 21:07:37 -0400

    wrote fill

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -6,9 +6,58 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(REFRESH)
+//starts at first pixel
+@SCREEN
+D=A
+@addr
+M=D
+
+//tests keyboard conditions
+@KBD
+D=M
+@WHITE
+D;JEQ
+@BLACK
+0;JMP
+
+(WHITE)
+@value
+M=0
+@DRAW
+0;JMP
+
+(BLACK)
+@value
+M=-1
+@DRAW
+0;JMP
+
+(DRAW)
+//draws the pixel
+@value
+D=M
+@addr
+A=M
+M=D
+
+//goes to the next pixel
+@addr
+M=M+1
+
+//checks if done
+D=M
+@KBD
+D=A-D
+@REFRESH
+D;JEQ
+
+//if not done, keep drawing
+@DRAW
+0;JMP
